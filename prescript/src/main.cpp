7a7aa923e3bf3f3b2aefc59bd51a12f8d8a94904
James Zheng <james.zheng@surreyschools.ca>
2026-02-02 15:41:34 -0800

    edited file

--- prescript/src/main.cpp
+++ prescript/src/main.cpp
@@ -2,15 +2,47 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <string>
+#include <vector>
+
+bool button_pressed = false;
+String phrases[4] = {"Hello World", "The Middle Never Forgets", "Gallop on, Rocinate!", "Fishmael"};
+String phrase;
+String obscured = phrase;
 
 Adafruit_SSD1306 display(128, 64, &Wire, -1);
-long rand;
 
+// generates the random stuff until the button is pressed
 void generate() {
-
+  for (int i = 0; i < phrase.length(); i++) {
+    long rand = random(33, 126);
+    obscured[i] = rand;
+  }
+  display.clearDisplay();
+  display.setCursor(0,0);
+  display.print(obscured);
+  display.display();
+  delay(150);
 }
-void prescript() {
 
+// reveals the prescript
+void reveal() {
+  for (int i = 0; i < phrase.length(); i++) {
+    for (int j = i; j < phrase.length(); j++) {
+      long rand = random(33,126);
+      obscured[j] = rand;
+    }
+    obscured[i] = phrase[i];
+
+    display.clearDisplay();
+    display.setCursor(0,0);
+    display.print(obscured);
+    display.display();
+    delay(150);
+  }
+  delay(1000);
+  phrase = phrases[random(1,4)];
+  button_pressed = false;
 }
 
 void setup() {
@@ -25,10 +57,14 @@ void setup() {
   display.clearDisplay();
   display.setTextColor(SSD1306_WHITE);
   display.setCursor(0,0);
-  display.write("blub");
+  display.setTextSize(1);
   display.display();
+  phrase = phrases[random(1, 4)];
 }
 
 void loop() {
-
+  generate();
+  if (digitalRead(18) && !button_pressed) {
+    reveal();
+  }
 }
